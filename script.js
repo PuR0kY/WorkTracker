3c624cb07c9b5e1721cd15253264097e125e0160
Matěj Pur <mattpur@seznam.cz>
2024-02-10 21:35:18 +0100

    revert

--- script.js
+++ script.js
@@ -78,21 +78,3 @@ window.onload = function(){
         saveAs(blob, "counters.txt");
     });
 }
-
-// Get references to the input and button elements
-const input = document.querySelector('.editable-button');
-const button = document.querySelector('.counter-button');
-
-// Add event listener to the input element to update button text
-input.addEventListener('input', function(event) {
-    button.textContent = event.target.value;
-});
-
-// Add event listener to handle the Enter key press
-input.addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
-        event.preventDefault(); // Prevent the default Enter key behavior
-        input.blur(); // Remove focus from the input field
-    }
-});
-
