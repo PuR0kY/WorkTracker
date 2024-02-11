b3939c36eb9cc5c9b0a1b76e5150bb9b12d45754
Matěj Pur <mattpur@seznam.cz>
2024-02-11 17:52:15 +0100

    v1

--- react-remake/work-tracker/src/Tabs.tsx
+++ react-remake/work-tracker/src/Tabs.tsx
@@ -1,27 +1,28 @@
+import React from 'react';
+
 export default function Tabs() {
-    return(
-        <nav className="nav">
-            <a href = "/" className="site-title">
-                Work Tracker
-            </a>
-            <ul>
-                <li>
-                    <a href="/Autorizace">Autorizace</a>
-                </li>
+  return (
+    <nav className="nav">
+      <a href="/" className="site-title">
+        Work Tracker
+      </a>
+      <ul>
+        <li>
+          <a href="/Autorizace">Autorizace</a>
+        </li>
 
-                <li>
-                    <a href="/SPEC">SPEC</a>
-                </li>
+        <li>
+          <a href="/SPEC">SPEC</a>
+        </li>
 
-                <li>
-                    <a href="/Reklamace">Reklamace</a>
-                </li>
+        <li>
+          <a href="/Reklamace">Reklamace</a>
+        </li>
 
-                <li>
-                    <a href="/Platby">Platby</a>
-                </li>
-            </ul>
-
-        </nav>
-    )
+        <li>
+          <a href="/Platby">Platby</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
